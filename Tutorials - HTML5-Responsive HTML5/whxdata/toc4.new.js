03ff75101068ab87c66d23599a923a0e61ef239a
Shubham <ssingh@semtech.com>
2024-06-26 16:30:17 +0530

    mangOH v1.0

---
+++ Tutorials - HTML5-Responsive HTML5/whxdata/toc4.new.js
@@ -0,0 +1,4 @@
+(function() {
+var toc =  [{"type":"item","name":"5.1.1 Configure your host terminal windows for development","url":"Tutorials_-_HTML5/4118961_Tut_DevelopApps/4118961_Tut_DevelopApps.htm#TOC_5_1_1_Configure_yourbc-2"},{"type":"item","name":"5.1.2 Develop and test applications","url":"Tutorials_-_HTML5/4118961_Tut_DevelopApps/4118961_Tut_DevelopApps.htm#TOC_5_1_2_Develop_and_testbc-3"}];
+window.rh.model.publish(rh.consts('KEY_TEMP_DATA'), toc, { sync:true });
+})();
